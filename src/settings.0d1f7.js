a3a15914321d334b4288cdcd4880992aede1e1ec
mcjj23 <hongyujieiris@gmail.com>
2021-11-25 18:04:48 +0800

    outer

---
+++ src/settings.0d1f7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/scene.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"1b082",resources:"64f42",main:"b2038"}};
